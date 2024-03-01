a2e124ebbda4d332744b75529009567c9d15e8dd
MuriloSG <muriloprojetosti@gmail.com>
2024-03-01 10:41:25 -0300

    Correction css

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,7 +2,7 @@ import {ReactNode } from "react";
 
 export function Container({ children }: { children: ReactNode }) { 
   return (
-    <div className="w-full max-w-7xl ms-auto bg-red-500">
+    <div className="w-full max-w-7xl mx-auto px-2">
       {children}
     </div>
   );
